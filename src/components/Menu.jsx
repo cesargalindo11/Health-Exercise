bedc1041577a61ddf2e1029100c4a9ecab3c7b33
cesargalindo11 <201407488@est.umss.edu>
2021-04-25 11:43:26 -0400

    pull formulario

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,6 +12,7 @@ const Menu = () => {
           <li className='nav-item'>
             <Link className='nav-link' to='/'>Inicio</Link>
           </li>
+          
           <li className='nav-item'>
             <Link className='nav-link' to='/login'>Login</Link>
           </li>
