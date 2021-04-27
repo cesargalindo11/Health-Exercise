098d246c4c848064a19d818deefb91fa2599090f
einarCA <201505054@est.umss.edu>
2021-04-27 00:46:35 -0400

    firebase

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -3,13 +3,12 @@ import 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDx102PBRftLlF4_ggcmkdABoKU2oyBG1M",
-  authDomain: "pruebasesion.firebaseapp.com",
-  projectId: "pruebasesion",
-  storageBucket: "pruebasesion.appspot.com",
-  messagingSenderId: "552503177778",
-  appId: "1:552503177778:web:1b461d3d47e460bc01022b",
-  measurementId: "G-SR9DX8MPTE"
+  apiKey: "AIzaSyCGJ1NrZdVV1_KecBNEKKu8aPqOTMZvD8w",
+    authDomain: "saludyejercicio-8966d.firebaseapp.com",
+    projectId: "saludyejercicio-8966d",
+    storageBucket: "saludyejercicio-8966d.appspot.com",
+    messagingSenderId: "892550091944",
+    appId: "1:892550091944:web:60a6a58f15ab3779879e9d"
 };
 
 const fb = firebase.initializeApp(firebaseConfig);
