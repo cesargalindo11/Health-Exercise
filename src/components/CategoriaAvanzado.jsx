bafa5c5c93a7f6ad26186f3081b308e4f0cfffd7
SergioSoliz <sergiosoliznogales@gmail.com>
2021-05-12 18:55:19 -0400

    Actucalizacion

---
+++ src/components/CategoriaAvanzado.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import '../assets/css/App.css';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from '../firebaseConfig';
+
+const CategoriaAvanzado = () => {
+    return (
+        <div>
+            <div class="container BotonesNivel p-3 my-3 border" >
+                <button
+                    className='barra btn btn-danger '>
+                    Atras
+                </button>
+            </div>
+            <div className="Categoria" class="container p-3 my-3 border">
+                <h1 style={{ color: 'white' }}>
+                    Categoria Avanzado
+                </h1>
+            </div>
+            <div class="container p-3 my-3 border" style={{ display: "flex", flexWrap: "wrap" }}>
+                <div class="card m-2">
+                    <img src='./CuerpoIntermedio.jpg' className='Ejercicios' />
+                    <button type="button" class="btn btn-info btn-block mt-4">Pecho</button>
+                </div>
+                <div class="card m-2">
+                    <img src='CuerpoIntermedio.jpg' className='Ejercicios' />
+                    <button type="button" class="btn btn-info btn-block mt-4">Espalda</button>
+                </div>
+                <div class="card m-2">
+                    <img src='./Categorias/BrazoAvanzado.jpg' className='Ejercicios' />
+                    <button type="button" class="btn btn-info btn-block mt-4">Brazo</button>
+                </div>
+                <div class="card m-2">
+                    <img src='./Categorias/PiernaAvanzado.jpg' className='Ejercicios' />
+                    <button type="button" class="btn btn-info btn-block mt-4">Pierna</button>
+                </div>
+                <div class="card m-2">
+                    <img src='CuerpoIntermedio.jpg' className='Ejercicios' />
+                    <button type="button" class="btn btn-info btn-block mt-4">Abdominal</button>
+                </div>
+                <div class="card m-2">
+                    <img src='CuerpoIntermedio.jpg' className='Ejercicios' />
+                    <button type="button" class="btn btn-info btn-block mt-4">Hombro</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default CategoriaAvanzado;
--- src/components/Niveles.jsx
+++ src/components/Niveles.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import {Link, useHistory} from 'react-router-dom';
+import '../assets/css/App.css';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from '../firebaseConfig';
+
 const Niveles = () => {
+  const historial = useHistory()
+  const [usuario, setUsuario] = useState(null)
 
   const CerrarSesion = () => {
 
@@ -10,10 +15,46 @@ const Niveles = () => {
 
   }
 
-  return(
+  return (
     <div>
-      <h1>Niveles</h1>
+
+      <div class="container BotonesNivel p-3 my-3 border" >
+        <button
+          //onClick={CerrarSesion}
+          className='barra btn btn-danger '>
+          Cerrar Sesion
+        </button>
+      </div>
+      <div class="container p-3 my-3 border">
+
+        <h1 style={{ color: 'white' }}>
+          Niveles
+        </h1>
+       
+        <div className='Niveles'>
+          <div class="container p-3 my-3 border">
+            <img src='./NivelesImagenes/CuerpoPrincipiante.jpg' className='CuerpoPrincipiante' />
+            <button type="button" class="btn btn-info btn-block mt-4">Nivel Principiante</button>
+          </div>
+          <div class="container p-3 my-3 border">
+            <img src='./NivelesImagenes/CuerpoIntermedio.jpg' className='CuerpoPrincipiante'></img>
+            <button type="button" class="btn btn-info btn-block mt-4">Nivel Intermedio</button>
+          </div>
+          <div class="container p-3 my-3 border">
+            <img src='./NivelesImagenes/CuerpoAvanzado.jpg' className='CuerpoPrincipiante'></img>
+            <button type="button" class="btn btn-info btn-block mt-4">Nivel Avanzado</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Niveles;
+/*
+<h1>Niveles</h1>
       {
+
           usuario
           ? ( <button
               onClick={CerrarSesion}
@@ -21,9 +62,16 @@ const Niveles = () => {
               Cerrar sesion
               </button> )
           : ( <span></span> )
-        }
-    </div>
-  )
-}
-
-export default Niveles;
+      }
+      <button
+              //onClick={CerrarSesion}
+              className='barra btn btn-danger '>
+              Perfil
+              </button>  
+              
+              
+              <button
+          //onClick={CerrarSesion}
+          className='barra btn btn-danger '>
+          Perfil
+        </button>*/
